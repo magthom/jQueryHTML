1300f541b13a464f0321ba02826ceffb3272e080
Maggie Thompson <maggiethompson@Maggies-MacBook-Air.local>
2020-05-27 10:24:30 -0500

    background color on mouse enter and leave

--- DOM/app.js
+++ DOM/app.js
@@ -4,13 +4,23 @@ document.addEventListener("DOMContentLoaded", function(){
     btn.appendChild(btnText);
     document.body.appendChild(btn);
 
-    let input = document.getElementById('name')
+    let input = document.getElementById('name');
     let button2 = document.getElementById('butt');
 
     btn.addEventListener('click', function (){
         alert('HI');
     });
 
+    let div = document.getElementById('div');
+
+div.addEventListener("mouseenter", function( event ) {   
+  event.target.style.backgroundColor = "purple";
+});
+
+div.addEventListener("mouseleave", function( event ) {
+    event.target.style.backgroundColor = "white";
+});
+     
     button2.addEventListener('click', function (){
         //let h1 = document.createElement('h1');
         //let h1Text = document.textContent = 'SUP?!';
