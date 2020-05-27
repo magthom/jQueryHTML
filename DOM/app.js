96d6e24b3c289b1abf800ac8951f7687fffd769b
Maggie Thompson <maggiethompson@Maggies-MacBook-Air.local>
2020-05-27 10:02:02 -0500

    text box alert

--- DOM/app.js
+++ DOM/app.js
@@ -4,10 +4,19 @@ document.addEventListener("DOMContentLoaded", function(){
     btn.appendChild(btnText);
     document.body.appendChild(btn);
 
+    let input = document.getElementById('name')
+    let button2 = document.getElementById('butt');
+
     btn.addEventListener('click', function (){
-        let h1 = document.createElement('h1');
-        let h1Text = document.textContent = 'SUP?!';
-        document.body.appendChild(h1);
-        alert (h1Text);
+        alert('HI');
+    });
+
+    button2.addEventListener('click', function (){
+        //let h1 = document.createElement('h1');
+        //let h1Text = document.textContent = 'SUP?!';
+        //document.body.appendChild(h1);
+        //alert (h1Text);
+        
+        alert(input.value);
     });
 });
